bd38b8b58ae935d05b34c4211f7335fb338d1dbd
Azizakmal <akmalprog@gmail.com>
2023-02-24 19:49:33 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      screens: {
+        sm: "380px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+      spacing: {
+        "big": "40rem",
+        "heig": "10rem",
+        "myleft": "50px"
+      }
+    },
+    fontFamily: {
+      nunito: ['Nunito', 'sans - serif']
+    }
+  },
+  plugins: [],
+}
